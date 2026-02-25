4a58222a20140d939ebef52ddd3a7ae9cfc7908d
ielbadisy <elbadisyimad@gmail.com>
2026-02-25 12:45:56 +0000

    Remove row bootstrap API

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -1,28 +1,4 @@
 library(testthat)
-test_that("bootstrap_tvrmst_diff_cols returns finite CI", {
-  t <- seq(0, 6, by = 1)
-  s_grid <- seq(0, 4, by = 1)
-  tau <- 2
-
-  S0_units <- rbind(
-    exp(-0.20 * t), exp(-0.22 * t), exp(-0.18 * t), exp(-0.21 * t), exp(-0.19 * t)
-  )
-  S1_units <- rbind(
-    exp(-0.15 * t), exp(-0.16 * t), exp(-0.14 * t), exp(-0.155 * t), exp(-0.145 * t)
-  )
-
-  set.seed(1)
-  out <- bootstrap_tvrmst_diff_cols(S1_units, S0_units, t, s_grid, tau, R = 30, eps = 1e-4)
-
-  expect_equal(names(out), c("s", "estimate", "lower", "upper"))
-  expect_equal(nrow(out), length(s_grid))
-  expect_true(all(is.finite(out$estimate)))
-  expect_true(all(is.finite(out$lower)))
-  expect_true(all(is.finite(out$upper)))
-  expect_true(all(out$lower <= out$upper))
-})
-
-
 test_that("bootstrap_tvrmst_diff_reps collapses for identical reps", {
   t <- seq(0, 6, by = 1)
   s_grid <- seq(0, 4, by = 1)
